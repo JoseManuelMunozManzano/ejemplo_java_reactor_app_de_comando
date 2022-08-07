9426ba1222ed5a1a53f68444473b3af425257bc2
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-07 16:01:36 +0200

    Creando flujos a partir de otros objetos

--- src/main/java/com/jmunoz/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/jmunoz/springboot/reactor/app/SpringBootReactorApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 // Para que la app sea de consola hay que implementar CommandLineRunner
 
@@ -22,12 +25,29 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// INMUTABILIDAD DE LOS OBSERVABLES
-		Flux<String> nombres = Flux.just("José Muñoz", "Adriana López", "María Pérez", "Tania Fernández",
-						"Ferney Rodríguez", "Bruce Lee", "Bruce Willis");
+		// CREANDO FLUJOS A PARTIR DE OTROS OBJETOS (no Flux.just)
+		// Por ejemplo, de una lista o un set que herede de Iterable se puede crear un flux.
+		// Esto es muy útil para usar por ejemplo con JPA al hacer una consulta a BD relacionales.
+		// Va a retornar una lista de objetos que la podemos convertir a un flujo (stream reactivo) y hacerle
+		// operaciones y nos podemos subscribir.
+		//
+		// Para BD no SQL como MongoDB o Cassandra, y ya con soporte reactivo, en vez de retornar un list,
+		// usando la API reactiva de Mongo va a retornar un flujo, Flux si son varios elementos o Mono si es uno.
+		// Ejemplo: https://docs.spring.io/spring-data/mongodb/docs/3.4.2/reference/html/#repositories
+		// y buscar flatMap
+		List<String> usuariosList = new ArrayList<>();
+		usuariosList.add("José Muñoz");
+		usuariosList.add("Adriana López");
+		usuariosList.add("María Pérez");
+		usuariosList.add("Tania Fernández");
+		usuariosList.add("Ferney Rodríguez");
+		usuariosList.add("Bruce Lee");
+		usuariosList.add("Bruce Willis");
 
-		// Todas estas transformaciones devuelven NUEVAS instancias en memoria y la última operación devuelve el valor
-		// en la variable usuarios.
+		// Se usa el método fromIterable de la clase Flux y se le pasa un Iterable.
+		// Hay otros métodos como fromArray o fromStream (un stream normal no reactivo lo pasamos a reactivo)
+		Flux<String> nombres = Flux.fromIterable(usuariosList);
+
 		Flux<Usuario> usuarios =  nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
 				.filter(usuario -> usuario.getNombre().equalsIgnoreCase("bruce"))
 				.doOnNext(usuario -> {
@@ -42,7 +62,6 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 					return usuario;
 				});
 
-		// Ahora se muestra la información de usuarios
 		usuarios.subscribe(e -> log.info(e.toString()),
 				error -> log.error(error.getMessage()),
 				new Runnable() {
