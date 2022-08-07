b8b782a5268dad8f2fc7b1df1dab47c30aa78b1d
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-07 15:10:26 +0200

    Subscribe con tarea y manejo de error

--- src/main/java/com/jmunoz/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/jmunoz/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,14 +1,19 @@
 package com.jmunoz.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
+
 // Para que la app sea de consola hay que implementar CommandLineRunner
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
@@ -17,13 +22,18 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// Trabajando con Flux para realizar nuestro primer Observable
-		Flux<String> nombres = Flux.just("José M", "Adriana", "Tania", "Ferney")
-		// Para implementar una tarea o proceso cada vez que el observador notifica al subscriber que se ha recibido
-		// un elemento se usa un elemento del ciclo de vida del observable llamado doOnNext() que es un método de
-		// evento.
-				.doOnNext(System.out::println);
+		Flux<String> nombres = Flux.just("José M", "Adriana", "", "Tania", "Ferney")
+				// Emulamos algún tipo de error
+				.doOnNext(e -> {
+					if (e.isEmpty()) {
+						throw new RuntimeException("Nombres no pueden ser vacíos");
+					}
+					System.out.println(e);
+				});
 
 		// Nos suscribimos al Observable
-		nombres.subscribe();
+		// En el subscriber también se pueden realizar tareas usando funciones anónimas al consumir la información.
+		// También se puede manejar cualquier tipo de error que pueda ocurrir en el segundo parámetro.
+		nombres.subscribe(log::info, error -> log.error(error.getMessage()));
 	}
 }
