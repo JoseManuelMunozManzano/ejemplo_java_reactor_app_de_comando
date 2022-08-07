6348af0702c428befb16ea738737c007fead6a33
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-07 15:29:53 +0200

    Operador map - Transformación a otro tipo de datos

--- src/main/java/com/jmunoz/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/jmunoz/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package com.jmunoz.springboot.reactor.app;
 
+import com.jmunoz.springboot.reactor.app.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -22,30 +23,24 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// Trabajando con Flux para realizar nuestro primer Observable
-		Flux<String> nombres = Flux.just("José M", "Adriana", "María", "Tania", "Ferney")
-				.map(nombre -> {return nombre.toUpperCase();})
-				// Emulamos algún tipo de error
-				.doOnNext(e -> {
-					if (e.isEmpty()) {
+		Flux<Usuario> nombres = Flux.just("José M", "Adriana", "María", "Tania", "Ferney")
+				// Usando map retornamos otro tipo de dato diferente. Ver la declaración Flux<Usuario>
+				.map(nombre -> new Usuario(nombre.toUpperCase(), null))
+				// Ahora tenemos que trabajar con tipo de datos Usuario
+				.doOnNext(usuario -> {
+					if (usuario == null) {
 						throw new RuntimeException("Nombres no pueden ser vacíos");
 					}
-					System.out.println(e);
+					System.out.println(usuario.getNombre());
 				})
-		// INMUTABLE
-		// Todas las operaciones que se realizan con los operadores NO MODIFICAN EL FLUJO ORIGINAL, sino que retornan
-		// una nueva instancia con los datos transformados.
-				.map(nombre -> {return nombre.toLowerCase();});
+				.map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;
+				});
 
-
-		// Nos suscribimos al Observable
-		// En el subscriber también se pueden realizar tareas usando funciones anónimas al consumir la información.
-		// También se puede manejar cualquier tipo de error que pueda ocurrir en el segundo parámetro.
-		//
-		// Tercer parámetro
-		// Hay otra forma del método subscribe que nos permite hacer una tarea cuando se completa
-		// la subscripción en el onComplete, que se implementa con un Runnable utilizando hilos.
-		// Es decir, cuando el Observable termina CORRECTAMENTE de emitir el último elemento.
-		nombres.subscribe(log::info,
+		// Tenemos usuarios
+		nombres.subscribe(e -> log.info(e.toString()),
 				error -> log.error(error.getMessage()),
 				new Runnable() {
 					@Override
